994d5c86c1cfd5e1bbb21bc512368a0b50a993bd
Leon Prouger <leonprou@gmail.com>
2018-08-28 15:01:10 +0300

    adding nametosymbol function with mocha test

--- client/src/utils/format.js
+++ client/src/utils/format.js
@@ -6,15 +6,17 @@ export const MAX_PRECISION = 18
 export const formatWei = (value) => new BigNumber(value).div(1e18).toFormat(ROUND_PRECISION, BigNumber.ROUND_HALF_UP)
 export const formatEther = (value) => new BigNumber(value).toFormat(ROUND_PRECISION, BigNumber.ROUND_HALF_UP)
 
+const pickLetters = (word, numOfLetters) => word.substr(0, numOfLetters).toUpperCase()
+
 export const nameToSymbol = (name) => {
   const words = name.trim(' ').split(/\s+/)
   if (words.length === 1) {
-    return words[0].substr(0, 3).toUpperCase()
+    return pickLetters(words[0], 3)
   } else if (words.length === 2) {
-    return (words[0].substr(0, 2) + words[1].charAt(0)).toUpperCase()
+    return pickLetters(words[0], 2) + pickLetters(words[1], 1)
   } else if (words.length === 3) {
-    return (words[0].charAt(0) + words[1].charAt(0) + words[2].charAt(0)).toUpperCase()
+    return pickLetters(words[0], 1) + pickLetters(words[1], 1) + pickLetters(words[2], 1)
   } else if (words.length >= 4) {
-    return (words[0].charAt(0) + words[1].charAt(0) + words[2].charAt(0) + words[3].charAt(0)).toUpperCase()
+    return pickLetters(words[0], 1) + pickLetters(words[1], 1) + pickLetters(words[2], 1) + pickLetters(words[3], 1)
   }
 }
