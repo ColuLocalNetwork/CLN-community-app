499eaea6c68c9673ce9f55121a8e726db8d5fd33
Leon Prouger <leonprou@gmail.com>
2018-08-28 11:45:51 +0300

    adding name to symbol function

--- client/src/utils/format.js
+++ client/src/utils/format.js
@@ -5,3 +5,16 @@ export const MAX_PRECISION = 18
 
 export const formatWei = (value) => new BigNumber(value).div(1e18).toFormat(ROUND_PRECISION, BigNumber.ROUND_HALF_UP)
 export const formatEther = (value) => new BigNumber(value).toFormat(ROUND_PRECISION, BigNumber.ROUND_HALF_UP)
+
+export const nameToSymbol = (name) => {
+  const words = name.trim(' ').split(/\s+/)
+  if (words.length === 1) {
+    return words[0].substr(0, 3).toUpperCase()
+  } else if (words.length === 2) {
+    return (words[0].substr(0, 2) + words[1].charAt(0)).toUpperCase()
+  } else if (words.length === 3) {
+    return (words[0].charAt(0) + words[1].charAt(0) + words[2].charAt(0)).toUpperCase()
+  } else if (words.length >= 4) {
+    return (words[0].charAt(0) + words[1].charAt(0) + words[2].charAt(0) + words[3].charAt(0)).toUpperCase()
+  }
+}
