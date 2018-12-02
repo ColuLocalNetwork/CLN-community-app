0277b6e9fa82d575df22a692ec8fb555a5cfe013
Leon Prouger <leonprou@gmail.com>
2018-12-02 13:41:48 +0200

    add to agregation option to filter by admin or user activity

--- server/src/routes/api/stats.js
+++ server/src/routes/api/stats.js
@@ -1,20 +1,46 @@
 const router = require('express').Router()
 const mongoose = require('mongoose')
 const Event = mongoose.model('Event')
+const communityModel = mongoose.community
 
-router.get('/:address', async (req, res, next) => {
+router.get('/:activityType/:address', async (req, res, next) => {
+  const activityType = req.params.activityType
+  const tokenAddress = req.params.address
+
   const interval = req.query.interval || 'month'
-
   if (interval !== 'month' && interval !== 'week') {
     throw Error('Bad interval parameter, correct values for interval is "month" or "week"')
   }
 
+  if (activityType !== 'user' && activityType !== 'admin') {
+    throw Error('Bad activityType parameter, correct values for activityType is "user" or "admin"')
+  }
+
+  const community = await communityModel.getByccAddress(tokenAddress)
+  const {owner} = community
+
+  const $match = {
+    eventName: 'Transfer',
+    address: tokenAddress
+  }
+
+  if (activityType === 'user') {
+    $match['user'] = {
+      $ne: [
+        {'returnValues.to': owner},
+        {'returnValues.from': owner}
+      ]
+    }
+  } else {
+    $match['$or'] = [
+      {'returnValues.to': owner},
+      {'returnValues.from': owner}
+    ]
+  }
+
   const stats = await Event.aggregate([
     {
-      $match: {
-        eventName: 'Transfer',
-        address: req.params.address
-      }
+      $match
     },
     {
       $group: {
