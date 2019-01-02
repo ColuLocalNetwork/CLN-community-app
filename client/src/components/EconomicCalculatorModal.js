1f2f177f27d7ae2536899488720a1f62df015682
inessakukushkina <inessakukushkina@gmail.com>
2019-01-02 18:52:13 +0300

    editing rule fore Add CLN in calculator

--- client/src/components/EconomicCalculatorModal.js
+++ client/src/components/EconomicCalculatorModal.js
@@ -80,6 +80,8 @@ class EconomicCalculatorModal extends Component {
       legend: {position: 'none'}
     }
 
+    console.log(this.props)
+
     return (
       <Modal className='calculator' onClose={this.props.hideModal}>
         <div className='metamask-popup-close' onClick={this.props.hideModal}>
@@ -184,9 +186,6 @@ class EconomicCalculatorModal extends Component {
             }
           </div>
           <div className='calculator-chart-footer'>
-            <a href='' className='calculator-chart-link'>
-              <FontAwesome name='play' /> Learn more about it
-            </a>
             <button className='btn-adding'>
               <FontAwesome name='plus' className='top-nav-issuance-plus' /> Add CLN
             </button>
