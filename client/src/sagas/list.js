df644a39b04225b63c70632bec0e44011de9ee1c
Leon Prouger <leonprou@gmail.com>
2019-01-02 15:00:29 +0200

    deploy contract

--- client/src/sagas/list.js
+++ client/src/sagas/list.js
@@ -1,15 +1,21 @@
-import { all } from 'redux-saga/effects'
-import { contract } from 'osseus-wallet'
+import { all, select } from 'redux-saga/effects'
+// import { contract } from 'osseus-wallet'
 
 import * as actions from 'actions/list'
 import {tryTakeEvery} from './utils'
 import SimpleListBytecode from 'constants/bytecode/SimpleList'
+import SimpleListAbi from 'constants/abi/SimpleList'
+import {getAccountAddress} from 'selectors/accounts'
+import web3 from 'services/web3'
 
 export function * createList () {
-  debugger
-  const SimpleListContract = contract.getContract({abiName: 'SimpleList'})
+  const SimpleListContract = new web3.eth.Contract(SimpleListAbi)
+  const accountAddress = yield select(getAccountAddress)
+
   const receipt = yield SimpleListContract.deploy({
     data: SimpleListBytecode
+  }).send({
+    from: accountAddress
   })
   console.log(receipt)
 }
