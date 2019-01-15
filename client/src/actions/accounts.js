41369fe67b1f587f414b4c7d3d1e8ce3d0fe220d
Leon Prouger <leonprou@gmail.com>
2019-01-15 13:50:02 +0200

    handling fuse chain

--- client/src/actions/accounts.js
+++ client/src/actions/accounts.js
@@ -1,13 +1,16 @@
 import {createRequestTypes, action} from './utils'
 
 export const BALANCE_OF_TOKEN = createRequestTypes('BALANCE_OF_TOKEN')
+export const BALANCE_OF_NATIVE = createRequestTypes('BALANCE_OF_NATIVE')
 export const BALANCE_OF_CLN = createRequestTypes('BALANCE_OF_CLN')
+
 export const FETCH_TOKENS = createRequestTypes('FETCH_TOKENS')
 export const FETCH_BALANCES = createRequestTypes('FETCH_BALANCES')
 export const FETCH_TOKENS_WITH_BALANCES = createRequestTypes('FETCH_TOKENS_WITH_BALANCES')
 export const SET_USER_INFORMATION = createRequestTypes('SET_USER_INFORMATION')
 
 export const balanceOfToken = (tokenAddress, accountAddress) => action(BALANCE_OF_TOKEN.REQUEST, {tokenAddress, accountAddress})
+export const balanceOfNative = (accountAddress) => action(BALANCE_OF_NATIVE.REQUEST, {accountAddress})
 export const balanceOfCln = (accountAddress) => action(BALANCE_OF_CLN.REQUEST, {accountAddress})
 
 export const fetchTokens = (accountAddress) => action(FETCH_TOKENS.REQUEST, {accountAddress})
--- client/src/containers/CLNFetcher.js
+++ client/src/containers/CLNFetcher.js
@@ -1,19 +1,16 @@
 import {Component} from 'react'
 import { connect } from 'react-redux'
 import {fetchClnToken} from 'actions/communities'
-import {fetchTokenQuote} from 'actions/fiat'
 
 class CLNFethcher extends Component {
   componentDidMount () {
     this.props.fetchClnToken()
-    this.props.fetchTokenQuote('CLN', 'USD')
   }
 
   render = () => null
 }
 
 const mapDispatchToProps = {
-  fetchTokenQuote,
   fetchClnToken
 }
 
