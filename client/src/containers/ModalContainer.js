4a225cb1795bf1f455f6e71d342bbadbdb2ca7ff
Leon Prouger <leonprou@gmail.com>
2019-01-21 16:03:30 +0200

    removing old events and adding new ones

--- client/src/containers/ModalContainer.js
+++ client/src/containers/ModalContainer.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux'
 
 import {hideModal} from 'actions/ui'
 import LoginModal from 'components/LoginModal'
-import ComingSoonModal from 'components/ComingSoonModal'
 import WrongNetworkModal from 'components/WrongNetworkModal'
 import LoadingModal from 'components/LoadingModal'
 import ExchangeModal from 'components/exchange/ExchangeModal'
@@ -11,18 +10,15 @@ import ErrorBoundary from 'components/ErrorBoundary'
 import UserDatatModal from 'components/issuance/UserDatatModal'
 import SimpleExchangeModal from 'components/oven/SimpleExchangeModal'
 import EconomicCalculatorModal from 'components/EconomicCalculatorModal'
-import ContractForm from 'components/ContactForm'
 
 import {
   LOGIN_MODAL,
-  SOON_MODAL,
   WRONG_NETWORK_MODAL,
   EXCHANGE_MODAL,
   LOADING_MODAL,
   USER_DATA_MODAL,
   SIMPLE_EXCHANGE_MODAL,
   ECONOMIC_CALCULATOR_MODAL,
-  CONTACT_FORM
 } from 'constants/uiConstants'
 
 const renderModal = (modalComponent, props) =>
@@ -32,14 +28,12 @@ const renderModal = (modalComponent, props) =>
 
 const MODAL_COMPONENTS = {
   [LOGIN_MODAL]: LoginModal,
-  [SOON_MODAL]: ComingSoonModal,
   [EXCHANGE_MODAL]: ExchangeModal,
   [WRONG_NETWORK_MODAL]: WrongNetworkModal,
   [LOADING_MODAL]: LoadingModal,
   [USER_DATA_MODAL]: UserDatatModal,
   [SIMPLE_EXCHANGE_MODAL]: SimpleExchangeModal,
-  [ECONOMIC_CALCULATOR_MODAL]: EconomicCalculatorModal,
-  [CONTACT_FORM]: ContractForm
+  [ECONOMIC_CALCULATOR_MODAL]: EconomicCalculatorModal
 }
 
 const ModalContainer = (props) => {
