df1ca94b9e9f4c892d2b3e58e93bf768c6d0fe36
Leon Prouger <leonprou@gmail.com>
2019-01-23 09:47:17 +0200

    delete import of old models

--- client/src/containers/ModalContainer.js
+++ client/src/containers/ModalContainer.js
@@ -5,19 +5,14 @@ import {hideModal} from 'actions/ui'
 import LoginModal from 'components/LoginModal'
 import WrongNetworkModal from 'components/WrongNetworkModal'
 import LoadingModal from 'components/LoadingModal'
-import ExchangeModal from 'components/exchange/ExchangeModal'
 import ErrorBoundary from 'components/ErrorBoundary'
 import UserDatatModal from 'components/issuance/UserDatatModal'
 import AddEntityModal from 'components/dashboard/AddEntityModal'
-import SimpleExchangeModal from 'components/oven/SimpleExchangeModal'
-
 import {
   LOGIN_MODAL,
   WRONG_NETWORK_MODAL,
-  EXCHANGE_MODAL,
   LOADING_MODAL,
   USER_DATA_MODAL,
-  SIMPLE_EXCHANGE_MODAL,
   ADD_DIRECTORY_ENTITY
 } from 'constants/uiConstants'
 
@@ -28,11 +23,9 @@ const renderModal = (modalComponent, props) =>
 
 const MODAL_COMPONENTS = {
   [LOGIN_MODAL]: LoginModal,
-  [EXCHANGE_MODAL]: ExchangeModal,
   [WRONG_NETWORK_MODAL]: WrongNetworkModal,
   [LOADING_MODAL]: LoadingModal,
   [USER_DATA_MODAL]: UserDatatModal,
-  [SIMPLE_EXCHANGE_MODAL]: SimpleExchangeModal,
   [ADD_DIRECTORY_ENTITY]: AddEntityModal
 }
 
