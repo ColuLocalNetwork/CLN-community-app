e0fc05082cc42d238c27d828f2b4ede4a640f3ff
Leon Prouger <leonprou@gmail.com>
2019-02-11 14:18:18 +0200

    reenable agenda

--- server/src/app.js
+++ server/src/app.js
@@ -49,7 +49,7 @@ require('./models')(mongoose)
 
 app.use(require('./routes'))
 
-// agenda.start()
+agenda.start()
 
 /// catch 404 and forward to error handler
 app.use(function (req, res, next) {
