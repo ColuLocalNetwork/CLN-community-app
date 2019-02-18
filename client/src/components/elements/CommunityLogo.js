a3e749f5251f5ff26cbc26ded8a7c6df98eabd54
inessakukushkina <inessakukushkina@gmail.com>
2019-02-18 12:45:49 +0300

    currency icons

--- client/src/components/elements/CommunityLogo.js
+++ client/src/components/elements/CommunityLogo.js
@@ -2,19 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Loader from 'components/Loader'
 
-const CommunityLogo = (props) => {
-  const communityLogo = props.metadata.communityLogo ? props.metadata.communityLogo.split('.')[0] : null
-  return (
-    <div className='coin-logo'>
-      {
-        props.metadata.communityLogo
-          ? <div className={`logo-img ${communityLogo}`} />
-          : <Loader color='#fff' className='logo-img' />
-      }
-      <span className='symbol-text'>{props.token.symbol}</span>
-    </div>
-  )
-}
+const CommunityLogo = (props) =>
+  <div className='coin-logo'>
+    {
+      props.metadata.communityLogo
+        ? <div className={`logo-img ${props.metadata.communityLogo}`} />
+        : <Loader color='#fff' className='logo-img' />
+    }
+    <span className='symbol-text'>{props.token.symbol}</span>
+  </div>
 
 CommunityLogo.propTypes = {
   token: PropTypes.object.isRequired,
