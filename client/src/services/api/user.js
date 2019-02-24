b611bf136e22548fed28e9b068f4515b24fa1cfe
Leon Prouger <leonprou@gmail.com>
2019-02-24 14:41:47 +0200

    change api server for user api calls

--- client/src/services/api/user.js
+++ client/src/services/api/user.js
@@ -1,17 +1,15 @@
 import request from 'superagent'
 
-const API_ROOT = CONFIG.api.url.default
-
 export const login = (apiRoot, {accountAddress, signature, date}) =>
   request.post(`${apiRoot}/users/login/${accountAddress}`)
     .send({signature, date})
     .then(response => response.body)
 
 export const addUser = (apiRoot, {user, tokenAddress, authToken}) =>
-  request.post(`${API_ROOT}/users`).send({user, tokenAddress})
+  request.post(`${apiRoot}/users`).send({user, tokenAddress})
     .set('Authorization', `Bearer ${authToken}`)
     .then(response => response.body)
 
 export const isUserExists = (apiRoot, {accountAddress}) =>
-  request.get(`${API_ROOT}/users/${accountAddress}`)
+  request.get(`${apiRoot}/users/${accountAddress}`)
     .then(response => response.body)
