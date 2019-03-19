935e03f2a738281aa9538ff87cc00b84634bd20f
Leon Prouger <leonprou@gmail.com>
2019-03-19 11:43:16 +0200

    delete old communitites

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,6 +1,5 @@
 module.exports = (mongoose) => {
   mongoose = mongoose || require('mongoose')
-  mongoose.community = mongoose.community || require('./Community')(mongoose)
   mongoose.event = mongoose.event || require('./Event')(mongoose)
   mongoose.tokenProgress = mongoose.tokenProgress || require('./TokenProgress')(mongoose)
   mongoose.token = mongoose.token || require('./Token')(mongoose)
--- server/src/routes/api/index.js
+++ server/src/routes/api/index.js
@@ -1,6 +1,5 @@
 const router = require('express').Router()
 
-router.use('/communities', require('./communities'))
 router.use('/metadata', require('./metadata'))
 router.use('/images', require('./images'))
 router.use('/receipts', require('./receipts'))
