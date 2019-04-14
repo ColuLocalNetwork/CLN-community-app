7ff89b243ccff4c36f610524cfdac21fab81780c
Lior <liorag@colu.com>
2019-04-14 16:10:42 +0300

    added loader when transfer || mint || butn

--- client/src/reducers/screens/token.js
+++ client/src/reducers/screens/token.js
@@ -1,9 +1,37 @@
-import { TRANSFER_TOKEN } from 'actions/token'
+import { TRANSFER_TOKEN, MINT_TOKEN, BURN_TOKEN } from 'actions/token'
+import { REQUEST, SUCCESS, FAILURE, PENDING, CONFIRMATION } from 'actions/constants'
 
-export default (state = {}, action) => {
+const initialState = {
+  transferTokenStatus: null,
+  mintBurnStatus: null
+}
+
+export default (state = initialState, action) => {
   switch (action.type) {
+    case TRANSFER_TOKEN.REQUEST:
+      return { ...state, transferTokenStatus: REQUEST, transferTokenInProgress: true }
+    case TRANSFER_TOKEN.CONFIRMATION:
+      return { ...state, ...action.response }
+    case TRANSFER_TOKEN.PENDING:
+      return { ...state, transferTokenStatus: PENDING }
     case TRANSFER_TOKEN.SUCCESS:
-      return {...state, ...action.response}
+      return { ...state, ...action.response, transferTokenInProgress: false }
+    case MINT_TOKEN.REQUEST:
+      return { ...state, mintBurnStatus: REQUEST, mintBurnToken: true }
+    case MINT_TOKEN.CONFIRMATION:
+      return { ...state, ...action.response }
+    case MINT_TOKEN.PENDING:
+      return { ...state, mintBurnStatus: PENDING }
+    case MINT_TOKEN.SUCCESS:
+      return { ...state, ...action.response, mintBurnToken: false }
+    case BURN_TOKEN.REQUEST:
+      return { ...state, mintBurnStatus: REQUEST, mintBurnToken: true }
+    case BURN_TOKEN.CONFIRMATION:
+      return { ...state, ...action.response }
+    case BURN_TOKEN.PENDING:
+      return { ...state, mintBurnStatus: PENDING }
+    case BURN_TOKEN.SUCCESS:
+      return { ...state, ...action.response, mintBurnToken: false }
     default:
       return state
   }
