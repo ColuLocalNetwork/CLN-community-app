bf780105fb2a4d1c9fa82e0e237b91a7c91cbfc1
Lior <liorag@colu.com>
2019-05-05 12:26:22 +0300

    linter

--- client/src/components/dashboard/MintBurnForm.jsx
+++ client/src/components/dashboard/MintBurnForm.jsx
@@ -13,7 +13,7 @@ export default class MintBurnForm extends PureComponent {
     super(props)
 
     const { balance } = this.props
-    
+
     this.initialValues = {
       actionType: '',
       mintAmount: '',
