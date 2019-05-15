c863a0b77d8da984a750402f185b5d722a127f12
Leon Prouger <leonprou@gmail.com>
2019-05-15 15:44:29 +0300

    update deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,15 @@
 
 set -e
 
+cd dapp
+
 if ! [ -n "$NO_INSTALL" ]
 then
   npm i lerna --registry https://registry.npmjs.org/
   npm run bootstrap --registry https://registry.npmjs.org/
 fi
+
+npm run build
 
 cp dist/* ../server/public
 
