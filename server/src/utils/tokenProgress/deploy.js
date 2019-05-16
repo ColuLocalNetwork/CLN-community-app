7846062c9f1ed764a654a80d7c8fe72a86359f26
Leon Prouger <leonprou@gmail.com>
2019-05-16 10:31:05 +0300

    save

--- server/src/utils/tokenProgress/deploy.js
+++ server/src/utils/tokenProgress/deploy.js
@@ -2,22 +2,31 @@ const mongoose = require('mongoose')
 const { deployBridge } = require('@utils/bridge')
 const { deployCommunity } = require('@utils/community')
 const { stepFailed, stepDone } = require('@utils/tokenProgress')
-const { transferOwnership } = require('@utils/token')
+const { transferOwnership, fetchTokenData } = require('@utils/token')
 const Token = mongoose.model('Token')
+const TokenProgress = mongoose.model('TokenProgress')
 
 const deployFunctions = {
+  tokenIssued: console.log,
   bridge: deployBridge,
   membersList: deployCommunity,
   transferOwnership: transferOwnership
 }
 
-const stepsOrder = ['bridge', 'membersList']
+const stepsOrder = ['tokenIssued', 'bridge', 'membersList']
 
 const mandatorySteps = {
   bridge: true
 }
 
-const deploy = async (tokenProgress, steps) => {
+const deploy = async (tokenAddress, steps) => {
+  if (steps.tokenIssued && steps.tokenIssued.isImported) {
+    await fetchTokenData()
+  }
+  const tokenProgress = await TokenProgress.findOne({ tokenAddress })
+  if (!tokenProgress || !tokenProgress.steps.tokenIssued) {
+    return stepFailed('tokenIssued', tokenAddress, 'No such token issued')
+  }
   const token = await Token.findOne({ address: tokenProgress.tokenAddress })
 
   if (!token) {
