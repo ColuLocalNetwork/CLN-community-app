83da405a1f01347ea8a3ec6f1ab1c5f1ba9a0da2
Leon Prouger <leonprou@gmail.com>
2019-06-13 15:42:28 +0300

    move tasks to tasks/events

--- server/src/tasks/index.js
+++ server/src/tasks/index.js
@@ -1,18 +1,7 @@
-const processPastEvents = require('./utils').processPastEvents
-const processPastTransferEvents = require('./transfer').processPastTransferEvents
-const processPastBridgeMappingEvents = require('./bridge').processPastBridgeMappingEvents
-const deploy = require('./deployment').deploy
-
-const TokenFactoryAbi = require('@fuse/token-factory-contracts/build/abi/TokenFactory')
-const { web3 } = require('@services/web3/foreign')
-const config = require('config')
-
-const tokenFactory = new web3.eth.Contract(TokenFactoryAbi, config.get('network.foreign.addresses.TokenFactory'))
-const processPastTokenCreatedEvents = processPastEvents.bind(null, 'TokenCreated', tokenFactory)
+const deployment = require('./deployment')
+const events = require('./deployment')
 
 module.exports = {
-  processPastTokenCreatedEvents,
-  processPastTransferEvents,
-  processPastBridgeMappingEvents,
-  deploy
+  ...events,
+  ...deployment
 }
